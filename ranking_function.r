378c8bc660227c521db35f945071398b82d68ff2
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-02-28 10:14:28 -0600

    cleaned up future work TODOs

--- ranking_function.r
+++ ranking_function.r
@@ -1,18 +1,6 @@
 ### Weighted Loss Function for Ranking by Position ###
 ## Cora Allen-Coleman Feb 2018 ##
 
-## Q: for rank, must use r function sort. How can we fix this? OR is this fine?
-
-## TODO test logit
-## TODO 3 outer product of the matrix to vectorize to replace double for loops (meeting) vectorizing apply outer product
-## So far, only been tested with relatively simple bayesian models. allow for stan model OR matrix of parameter samples ##
-
-## TODO loss for zero one loss
-
-## TODO should there be autoscaling of original matrix to prevent numbers that are too small
-## TODO add checks for invalid inputs
-## TODO question: what should epsilon be? When do we have numerical stability problem
-
 library(rstan)
 library(clue)
 
@@ -60,16 +48,12 @@ WeightedLossRanking <- function(model = NULL, parameter = NULL, sampleMatrix = N
 ranks <- weight_loss_ranking(rand_int_model, parameter = "p", loss = 2) #model case
 ranks <- weight_loss_ranking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
 
-## Possible Weights ##
-unequal <- rep(1, times = 21); unequal[4:9] <- 3
-
-## Ranked Data Frame ##
+## Ranked Data Frame Output ##
 County <- raw_data0[,c(3)]
 rankedDataFrame <- as.data.frame(County)
 rankedDataFrame$p <- raw_data0[,4]/raw_data0[,5]*100
 rankedDataFrame$rank <- as.integer(ranks)
 library(dplyr); arrange(rankedDataFrame, rank)
-
 
 ##Example Data ##
 raw_data0 <- read.csv("/Users/cora/git_repos/RankingMethods/data/LBW.csv", header = TRUE)
@@ -87,25 +71,34 @@ data = list(
 )
 ## Create Model with Random Intercepts for Each County ##
 rand_int_model <- stan(file="/Users/cora/git_repos/RankingMethods/randInt.stan",data=data, seed = 10)
-i_samples <- rstan::extract(model, pars=parameter)[[1]]
 
+## sampleMatrix input ##
+i_samples <- rstan::extract(model, pars=parameter)[[1]] 
 
-
-#LSAP might act weirdly with actual 0 weights (this is a problem with Louis)
-#what we really want is orders of magnitude between weights
-#these weights dont need to sum to one (to normalize, divide each by sum)
-#c(1, e, e^2, e^3) then normalize if you want. TODO question: what should epsilon be? When do we have numerical stability problem
+## Possible Weights ##
+unequal <- rep(1, times = 21); unequal[4:9] <- 3
 
 ## Future Work Notes ##
-#TODO bring in heuristic methods for LSAP look for papers by Louis' group 
-# TODO question: does our 1, e, e^2 etc work better than 1, 1, 1, 0, 0, 0, (these zeros will all be tied). 
-# Epsilon losses automatically breaks ties. Mostly want to show that it doesnt change answer about top 10 + ranking of bottom set will be better.
-# Next question: do I really wanmt to have cliffs? Should it be smooth, gradual? How would we make this smooth? Are there various ways to try to get the good points of cliff functions.
-# Can you make a smooth function that performs as well as cliffs?
-# Is there a natural cliff? (page views on google?) Then is this related to threshold functions? e.g. i just care about my item, everyone else is less important
-# smoothness might be desireable in situations where you see the whole list. (college rankings) 
+## TODO test logit
+## TODO loss for zero one loss
 
-#TODO idea for future: D3.js? this could be a good situation to use weights based on people's interests. Show different weight vectors.
+## TODO 3 outer product of the matrix to vectorize to replace double for loops. vectorizing apply outer product
+## TODO Q: for rank, must use r function sort. How can we fix this? OR is this fine?
+## TODO should there be autoscaling of original matrix to prevent numbers that are too small?
+## TODO add checks for invalid inputs
+## TODO weights question: what should epsilon be? When do we have numerical stability problem
 
+## TODO bring in heuristic methods for LSAP look for papers by Louis' group 
+## TODO idea for visualizations: D3.js? this could be a good situation to use weights based on people's interests. Show different weight vectors.
 
-##DONE:
+## TODO Test different weighting strategies
+##  LSAP might act weirdly with actual 0 weights (this is a problem with Louis)
+##  what we really want is orders of magnitude between weights. (weights dont need to sum to one. To normalize, divide each by sum)
+##  e.g. c(1, e, e^2, e^3) then normalize if you want. TODO question: what should epsilon be? When do we have numerical stability problem
+##  Q: does our 1, e, e^2 etc work better than 1, 1, 1, 0, 0, 0, (these zeros will all be tied). 
+##  Epsilon losses automatically breaks ties. Mostly want to show that it doesnt change answer about top 10 + ranking of bottom set will be better.
+##  Q: do I really want to have cliffs? Should it be smooth, gradual? How would we make this smooth? Are there various ways to try to get the good points of cliff functions.
+##  Can you make a smooth function that performs as well as cliffs? Is there a natural cliff? (page views on google?) 
+##  Then is this related to threshold functions? e.g. i just care about my item, everyone else is less important
+##  Smoothness might be desireable in situations where you see the whole list. (college rankings) 
+
