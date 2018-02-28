9cfd9378bc3a85503706949325d8b1f1b5484b01
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-02-28 10:31:40 -0600

    added zero one loss case. it's still not working correctly

--- ranking_function.r
+++ ranking_function.r
@@ -14,7 +14,7 @@ WeightedLossRanking <- function(model = NULL, parameter = NULL, sampleMatrix = N
 #   model: a stan model for the estimates
 #   parameter: parameter to rank, as a string. Only necessary if inputting model rather than sampleMatrix.
 #   sampleMatrix: a matrix of samples. dim = n samples by n items
-#   loss: an exponent indicating the loss function for ranking. options: 2=square, 1=absolute, zero? TODO
+#   loss: an exponent indicating the loss function for ranking. options: 2=square, 1=absolute, 0=zero
 #   f: scale for loss calculation. options: identity, rank (input R's sort function for now) TODO
 #   rankweights: a vector of length equal to number of items to be ranked. Weights positions.
 #   itemweights: a vector of length equal to number of items to be ranked. Weights items.
@@ -34,20 +34,28 @@ WeightedLossRanking <- function(model = NULL, parameter = NULL, sampleMatrix = N
   
   n <- ncol(i) #n = # items to be ranked
   
-  #calculate loss 
-  LossRnk <- matrix(NA,n,n)
-  for (i in 1:n) {
-    for (j in 1:n) {
-      LossRnk[i,j] <- rankweights[j]*itemweights[i]*mean(abs((rho_i[,i]-rho_j[j,]))^loss) #rankweights[j]*itemweights[i]*mean(abs((ranks[i,]-j))^loss) for rank position weighting. for both, just multiply
+  if (loss == 0){ #zero one loss case
+    LossRnk <- matrix(NA,n,n)
+    for (i in 1:n) {
+      for (j in 1:n) {
+        LossRnk[i,j] <- rankweights[j]*itemweights[i]*mean(rho_i[,i]!=rho_j[j,])
+      }
     }
+    return(solve_LSAP(LossRnk))
+  } else{ #other loss cases
+    LossRnk <- matrix(NA,n,n)
+    for (i in 1:n) {
+      for (j in 1:n) {
+        LossRnk[i,j] <- rankweights[j]*itemweights[i]*mean(abs((rho_i[,i]-rho_j[j,]))^loss)
+      }
+    }
+    return(solve_LSAP(LossRnk))
   }
-  return(solve_LSAP(LossRnk))
 }
 
 ## Testing Function on Example Data (below) ##
-require(boot)
-ranks <- weight_loss_ranking(rand_int_model, parameter = "p", loss = 2, f = inv.logit) #model case
-ranks <- weight_loss_ranking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
+ranks <- weight_loss_ranking(rand_int_model, parameter = "p", loss = 2) #model case
+#ranks <- weight_loss_ranking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
 
 ## Ranked Data Frame Output ##
 County <- raw_data0[,c(3)]
@@ -80,18 +88,16 @@ i_samples <- rstan::extract(model, pars=parameter)[[1]]
 unequal <- rep(1, times = 21); unequal[4:9] <- 3
 
 ## Future Work Notes ##
-## TODO loss for zero one loss
-
 ## TODO 3 outer product of the matrix to vectorize to replace double for loops. vectorizing apply outer product
+## TODO loss for zero one loss
 ## TODO Q: for rank, must use r function sort. How can we fix this? OR is this fine?
 ## TODO should there be autoscaling of original matrix to prevent numbers that are too small?
 ## TODO add checks for invalid inputs
-## TODO weights question: what should epsilon be? When do we have numerical stability problem
 
-## TODO bring in heuristic methods for LSAP look for papers by Louis' group 
-## TODO idea for visualizations: D3.js? this could be a good situation to use weights based on people's interests. Show different weight vectors.
+## TODO Add heuristic methods for LSAP + compare. Look for papers by Louis' group 
 
-## TODO Test different weighting strategies
+## TODO Compare weighting strategies.
+##  Q: what should epsilon be? When do we have numerical stability problems?
 ##  LSAP might act weirdly with actual 0 weights (this is a problem with Louis)
 ##  what we really want is orders of magnitude between weights. (weights dont need to sum to one. To normalize, divide each by sum)
 ##  e.g. c(1, e, e^2, e^3) then normalize if you want. TODO question: what should epsilon be? When do we have numerical stability problem
@@ -101,4 +107,6 @@ unequal <- rep(1, times = 21); unequal[4:9] <- 3
 ##  Can you make a smooth function that performs as well as cliffs? Is there a natural cliff? (page views on google?) 
 ##  Then is this related to threshold functions? e.g. i just care about my item, everyone else is less important
 ##  Smoothness might be desireable in situations where you see the whole list. (college rankings) 
+
+## TODO create visualizations: D3.js? this could be a good situation to use weights based on people's interests. Show different weight vectors.
 
