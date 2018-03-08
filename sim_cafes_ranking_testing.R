01c222f363f0f489b79ee63590fcbcf76dbfca06
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-03-08 10:24:45 -0600

    created plots to show stochastic ordering

--- sim_cafes_ranking_testing.R
+++ sim_cafes_ranking_testing.R
@@ -53,45 +53,16 @@ p2 <- stack(p2)
 p2$cafe <- p2$ind
 ggplot(p2, aes(x = values, color = cafe, fill = cafe, alpha = 0.1)) + geom_density()
 
-
 #Equal Variance Example with Graph
-x <- data.frame(item1=rnorm(10000, 0, 1), item2=rnorm(10000,1,1), item3=rnorm(10000,2, 1))
 library(ggplot2);library(reshape2)
+x <- data.frame(item1=rnorm(10^7, 0, 2), item2=rnorm(10^7,1,2), item3=rnorm(10^7,2, 2))
 data<- melt(x)
-ggplot(data,aes(x=value, fill=variable)) + geom_density(alpha=0.3)
+equalVar <- ggplot(data,aes(x=value, fill=variable)) + geom_density(alpha=0.3)
+ggsave(filename = "/Users/cora/Dropbox/UW-Madison/CurrentResearch/Ranking/presentations/StudentSeminarMarch2018/images/equal_var.png", plot = equalVar)
 
 #Unequal Variance Example
-x <- data.frame(item1=rnorm(10000, 0, 1), item2=rnorm(10000,2,1), item3=rnorm(10000,2, 4))
-library(ggplot2);library(reshape2)
+x <- data.frame(item1=rnorm(10^7, 0, 2), item2=rnorm(10^7,1,2), item3=rnorm(10^7,2, 4))
 data<- melt(x)
-ggplot(data,aes(x=value, fill=variable)) + geom_density(alpha=0.3)
+unequalVar <- ggplot(data,aes(x=value, fill=variable)) + geom_density(alpha=0.3)
+ggsave(filename = "/Users/cora/Dropbox/UW-Madison/CurrentResearch/Ranking/presentations/StudentSeminarMarch2018/images/unequal_var.png", plot = unequalVar)
 
-
-
-
-ggplot(data,aes(x=value, fill=variable)) + geom_histogram(alpha=0.25)
-ggplot(data,aes(x=variable, y=value, fill=variable)) + geom_boxplot()
-
-#create stochastically ordered data set
-cafes <- as.data.frame(matrix((seq(from = 1, to = 5, by = 1)), ncol = 1))
-colnames(cafes) <- c("cafe")
-cafes$true_p <- seq(from = 0.5, to = 0.7, by = 0.05) #levels of true p
-cafes$attempts <- c(rep(100000, times = nrow(cafes))) #create lots of variation here
-cafes$SuccessfulConnections <- rbinom(n = 5, size = cafes$attempts, cafes$true_p)
-cafes$sim_p <- cafes$SuccessfulConnections/cafes$attempts
-ggplot(cafes, aes(x = sim_p, color = cafe, fill = cafe, alpha = 0.1)) + geom_density()
-
-
-## sim data + model ##
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-sim_data = list(
-  J = nrow(cafes), #should be 12
-  n = with(cafes, attempts),
-  count = with(cafes, SuccessfulConnections),
-  cafe = with(cafes,as.integer(as.factor(cafe)))
-)
-## Create Model with Random Intercepts for Each County ##
-sim_rand_int_model <- stan(file="/Users/cora/git_repos/RankingMethods/sim_randInt.stan",data=sim_data, seed = 10)
-
-p <- extract(sim_rand_int_model, pars = "p")[[1]]
