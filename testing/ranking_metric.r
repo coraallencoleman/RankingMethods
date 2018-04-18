9bfd3f82728861bbcf18f8c5d03eb9d7c2ec637b
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-04-18 09:11:39 -0500

    drafted ranking metric for binomial models

--- testing/ranking_metric.r
+++ testing/ranking_metric.r
@@ -1,1 +1,43 @@
 # for use with simulated data
+library(dplyr)
+library(clue)
+library(rstan)
+#cafes data metric
+
+## Cafe Ranked Data Frame Output ## (from raking_testing.r)
+
+
+##function metric to see if our top five matches true top five
+#true top five:
+cafes <-cafes %>% dplyr::arrange(desc(true_p), desc(attempts))
+
+sim_ranks <- WeightedLossRanking(model = sim_rand_int_model, parameter = "p", f = rank, loss = 2, lossTotal = TRUE)
+
+rankedCafes <- as.data.frame(cafes)
+rankedCafes$true_p <- cafes$true_p*100
+rankedCafes$rank <- as.integer(sim_ranks) #this ranks lowest to highest
+arrange(rankedCafes, desc(rank))
+rankedCafes <- rankedCafes %>% dplyr::arrange(desc(rank))
+rankedCafes[1:5,]$cafe == cafes[1:5,]$cafe
+
+TopRankMetric <- function(rankObject, originalDataFrame){
+  # function metric to see if our top five matches true top five for Binomial model
+  #   
+  # Args:
+  #   rankObject: an output of WeightedLossRanking
+  #   originalDataFrame: a data frame with column of true probabilities, true N (column names must match p, n)
+  #
+  # Returns:
+  #   logical vector
+  #
+  # Dependencies: rstan, clue, dplyr
+  
+  originalDataFrame <-originalDataFrame %>% dplyr::arrange(desc(p), desc(n)) 
+  
+  rankedCafes <- as.data.frame(originalDataFrame)
+  rankedCafes$true_p <- cafes$true_p*100
+  rankedCafes$rank <- as.integer(sim_ranks) #this ranks lowest to highest
+  arrange(rankedCafes, desc(rank))
+  rankedCafes <- rankedCafes %>% dplyr::arrange(desc(rank))
+  rankedCafes[1:5,]$cafe == cafes[1:5,]$cafe
+}
--- testing/ranking_testing.r
+++ testing/ranking_testing.r
@@ -2,69 +2,27 @@
 #Step 0: Load
 library(rstan)
 library(dplyr)
+library(dplyr)
 #run entire ranking_function.r file. This puts WeightedLossRanking from ranking_function.r into environment and adds packages
 set.seed(10)
-
-#STEP 2
-##Testing for small Normal data ##
-small_normal_ranks <- WeightedLossRanking(normal_model, parameter = "alpha", loss = 2)
-#estimating five means for each of the five items
-
-##Testing for Normal Two Level Model Data ## 
-norm_ranks <- WeightedLossRanking(normal_model, parameter = "alpha", loss = 2)
-#expected rank? Might not be useful to do this (too long)
-
-## Testing Function on Example County Data ##
-ranks <- WeightedLossRanking(NJ_rand_int_model, parameter = "p", loss = 2) #TODO loss = 0 doesnt work
-#ranks <- WeightedLossRanking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
-## County n = 21Ranked Data Frame Output ##
-County <- raw_data0[,c(3)]
-rankedDataFrame <- as.data.frame(County)
-rankedDataFrame$p <- raw_data0$NumLBW/raw_data0$NumBirths*100
-rankedDataFrame$rank <- as.integer(ranks)
-library(dplyr); arrange(rankedDataFrame, rank)
-
-## Testing Function on Illinois County Data ##
-ranks <- WeightedLossRanking(IL_rand_int_model, parameter = "p", loss = 2)
-#ranks <- WeightedLossRanking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
-## County n = 21Ranked Data Frame Output ##
-County <- raw_data_I$County
-rankedDataFrame <- as.data.frame(County)
-rankedDataFrame$p <- raw_data_I$NumLBW/raw_data_I$NumBirths*100
-rankedDataFrame$rank <- as.integer(ranks)
-library(dplyr); arrange(rankedDataFrame, rank) #lots of disagreement here. 
-#TODO compare for different types of ranking
-
-
 
 #STEP 1: Read in data + create models 
 ## Binomial Random Intercept n = 10 with conflicts ##
 #simulate county-like data
 cafes <- as.data.frame(matrix((seq(from = 1, to = 12, by = 1)), ncol = 1))
 colnames(cafes) <- c("cafe")
-cafes$true_p <- rep(c(.6, .7, .8, .9), each = 3) #four levels of true p
-cafes$attempts <- c(rep(c(10, 100, 1000), times = 4)) #create lots of variation here
-cafes$SuccessfulConnections <- rbinom(n = 12, size = cafes$attempts, cafes$true_p)
+cafes$p <- rep(c(.6, .7, .8, .9), each = 3) #four levels of true p
+cafes$n <- c(rep(c(10, 100, 1000), times = 4)) #create lots of variation here
+cafes$SuccessfulConnections <- rbinom(n = 12, size = cafes$n, cafes$p)
 ## sim data + model ##
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 sim_data = list(
   J = nrow(cafes), #should be 12
-  n = with(cafes, attempts),
+  n = with(cafes, n),
   count = with(cafes, SuccessfulConnections),
   cafe = with(cafes,as.integer(as.factor(cafe)))
 )
-
-## Create Model with Random Intercepts for Each Cafe ##
-sim_rand_int_model <- stan(file="/Users/cora/git_repos/RankingMethods/sim_randInt.stan",data=sim_data, seed = 10)
-#get posterior means from stan model
-#get credible intervals from stan model
-## Cafe Ranked Data Frame Output ##
-sim_ranks <- WeightedLossRanking(model = sim_rand_int_model, parameter = "p", f = rank, loss = 2, lossTotal = TRUE)
-rankedCafes <- as.data.frame(cafes)
-rankedCafes$true_p <- cafes$true_p*100
-rankedCafes$rank <- as.integer(sim_ranks) #this ranks lowest to highest
-rankedCafes<-arrange(rankedCafes, desc(rank))
 
 ## Example County Data n = 21 ## 
 raw_data0 <- read.csv("/Users/cora/git_repos/RankingMethods/data/LBW.csv", header = TRUE)
@@ -118,3 +76,45 @@ math_data = list(
   school = with(math,as.integer(as.factor(School))) #goes in numeric order, but shifts them up so that all starting at 1
 )
 normal_model <- stan(file="/Users/cora/git_repos/RankingMethods/normal_two_level.stan",data=math_data, seed=10)
+
+#STEP 2
+##Testing for small Normal data ##
+small_normal_ranks <- WeightedLossRanking(normal_model, parameter = "alpha", loss = 2)
+#estimating five means for each of the five items
+
+##Testing for Normal Two Level Model Data ## 
+norm_ranks <- WeightedLossRanking(normal_model, parameter = "alpha", loss = 2)
+#expected rank? Might not be useful to do this (too long)
+
+## Testing Function on Example County Data ##
+ranks <- WeightedLossRanking(NJ_rand_int_model, parameter = "p", loss = 2) #TODO loss = 0 doesnt work
+#ranks <- WeightedLossRanking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
+## County n = 21Ranked Data Frame Output ##
+County <- raw_data0[,c(3)]
+rankedDataFrame <- as.data.frame(County)
+rankedDataFrame$p <- raw_data0$NumLBW/raw_data0$NumBirths*100
+rankedDataFrame$rank <- as.integer(ranks)
+library(dplyr); arrange(rankedDataFrame, rank)
+
+## Testing Function on Illinois County Data ##
+ranks <- WeightedLossRanking(IL_rand_int_model, parameter = "p", loss = 2)
+#ranks <- WeightedLossRanking(sampleMatrix = i_samples, parameter = "p", loss = 2) #sample matrix case
+## County n = 21Ranked Data Frame Output ##
+County <- raw_data_I$County
+rankedDataFrame <- as.data.frame(County)
+rankedDataFrame$p <- raw_data_I$NumLBW/raw_data_I$NumBirths*100
+rankedDataFrame$rank <- as.integer(ranks)
+arrange(rankedDataFrame, rank) #lots of disagreement here. 
+#TODO compare for different types of ranking
+
+##Test function on cafe data
+## Create Model with Random Intercepts for Each Cafe ##
+sim_rand_int_model <- stan(file="/Users/cora/git_repos/RankingMethods/sim_randInt.stan",data=sim_data, seed = 10)
+#get posterior means from stan model
+#get credible intervals from stan model
+## Cafe Ranked Data Frame Output ##
+sim_ranks <- WeightedLossRanking(model = sim_rand_int_model, parameter = "p", f = rank, loss = 2, lossTotal = TRUE)
+rankedCafes <- as.data.frame(cafes)
+rankedCafes$p <- cafes$p*100
+rankedCafes$rank <- as.integer(sim_ranks) #this ranks lowest to highest
+arrange(rankedCafes, desc(rank))
