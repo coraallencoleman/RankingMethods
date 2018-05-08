080b9633d71a90a4e02da07c79db52c7d27c1aaa
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-05-08 16:14:20 -0500

    cleaned sim_ranking_experiment.r files

--- sim_ranking_experiment.r
+++ sim_ranking_experiment.r
@@ -1,14 +1,10 @@
-#Testing for WeightedLossRanking function
-
-##MAIN GOAL OF THIS EXPERIMENT
-##increase gaps in parameters until they're trivial. decrease until broken/impossible. 
-#think of this as an experiment. what experimental conditions do we need to run to get a sense of behavior.
+#Simulation/Experiments Testing for WeightedLossRanking function
 
 #Step 0: Load
 library(rstan)
 library(dplyr)
 library(rstanarm)
-#AND run entire ranking_function.r, ranking_metric.r files. 
+#AND run ranking_function.r, ranking_metric.r files. 
 set.seed(10)
 
 SelectNP <- function(N = 25, a_p = 1, b_p = 1, n_min = 10, n_max = 30, a_n = 1, b_n = 1,
@@ -136,76 +132,14 @@ RunSimulation <- function(N = 25, a_p = 1, b_p = 1, n_min = 10, n_max = 30, a_n 
 
 RunSimulation()
 
-#RANKING EVALUATION: use RankMetric for now
-
-#STEP 1: Simulate Different Types of Data
-## Binomial Random Intercept n = 100 ## EVEN GAPS
-#increase gaps until trivial. decrease until broken/impossible.
-## PARAMETERS ##
-gaps <- c(0.005, 0.01, 0.1) #gap sizes tested here
-topN = 10
-
-#initialize arrays and lists
-even <- array(data = NA, dim=c(length(gaps),4,10001)) #3 dim array with 3 matrices, 4 col and N rows (max = 10001) each
-even_model <- vector("list", length(gaps)) #list of rank objects for each of the 4 matrices
-even_rank <- vector("list", length(gaps))
-
-#do one simulation scenario at a time TODO
-
-even_metric_results <- #rank_metric results for each of the matrices
-  data.frame(metric_results = logical(length = topN))
-
-for (i in 1:length(gaps)){ #length(gaps)
-  N = length(seq(from = 0, to = 1, by = gaps[i]))
-  even[i, 1, 1:N] <- seq(from = 1, to = N, by = 1) #ITEM
-  even[i, 2, 1:N] <- seq(from = 0, to = 1, by = gaps[i]) #P
-  even[i, 3, 1:N] <- rep(as.integer(25),times = N) #SIZE
-  even[i, 4, 1:N] <- rbinom(n = N, size = even[i, 3, 1:N], even[i, 2, 1:N]) #SIM SUCCESSES 
-  #TODO need to do this for n simulation (n_sim = 1) 
-  #do this for each simulation sample data -> rankings one at a time 
-  #so that you dont have to do keep posteriors in memory
-  #TODO set num of posterior samples to smaller
-  #TODO ending array FOR EACH DATA SET is 3D N, nSim nRankingMethods or a list using method as names of the list
-  
-  
-  ## DATA FOR STAN##
-  rstan_options(auto_write = TRUE)
-  options(mc.cores = parallel::detectCores())
-  sim_data = list(
-    N = N, #N or numRows
-    item = even[i, 1, 1:N], #ITEM ID
-    sizeN = as.integer(even[i, 3, 1:N]), #same as cafes$n #SIZE
-    count = as.integer(even[i, 4, 1:N]) #SIM SUCCESSES
-  ) 
-  #MODEL
-  even_model[[i]] <- stan(file="/Users/cora/git_repos/RankingMethods/sim_randInt.stan",data=sim_data, seed = 10)
-  
-  even_rank[[i]] <- WeightedLossRanking(model = even_model[[i]], parameter = "p", loss = 2, lossTotal = TRUE)
-  
-  #compare using rankMetric, add to dataframe of results
-  nam <- paste("gap", i, sep = "")
-  even_metric_results$new <- RankMetric(even_rank[[i]], even[i, 1:4, 1:N], order = "largest", topN = topN)
-  colnames(even_metric_results)[i+1] <- nam
-}
-
-#save results to a file
-write.csv(even_metric_results, file = "/Users/cora/git_repos/RankingMethods/results/even_gaps_RankMetric_results.csv")
-
-#TODO DO THIS OVER DIFFERENT LOSSES
-
-#TODO repeat above with uneven gap size
-#try with bigger gaps, random unif on a range to have arbitrary gaps
-#increase gaps until trivial. decrease until broken/impossible. 
-
-#to make this flexible for nonequal gap size
-qbeta(1:N/(N+1), 1, 1) #but then vary the last two parameters for variable gap size
-#then change N, a, b.
-
-#TODO repeat with testing over variation in N
-
-#TODO add weights. How does this compare to not weighting at all?
+##Main Questions for this Experiment:
+##increase gaps in parameters until they're trivial. decrease until broken/impossible. 
+#think of this as an experiment. what experimental conditions do we need to run to get a sense of behavior?
 
 ### Simulations TODO
 # function to save sim results to file
 # even gaps
-# uneven gaps
+# uneven gaps #qbeta(1:N/(N+1), 1, 1). Vary the last two parameters for variable gap size
+# different losses
+# weights vs no weights
+# variation in N
