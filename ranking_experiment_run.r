e5d185f11eb155de7d853b2e1de7cf98a7f047c6
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-05-14 14:42:48 -0500

    ranking experiment working except that zero one loss doesnt work

--- ranking_experiment_run.r
+++ ranking_experiment_run.r
@@ -9,16 +9,18 @@
 # uneven gaps #qbeta(1:N/(N+1), 1, 1). Vary the last two parameters for variable gap size
 # different losses
 # weights vs no weights (part of ranking)
-# variation in N
+# variation in N #for (n in c(10, 15, 20)){#number
 # do this simulation on rank, logit scales
 
 
-for (i in c()){#ranking options
-  RunSimulation(N = 10, a_p = 1, b_p = 1, n_min = 10, n_max = 30, a_n = 1, b_n = 1, #data
+for (n in c(20)){ #numItems
+  for (l in c(1, 2)){ #loss types
+  RunSimulation(N = n, a_p = 1, b_p = 1, n_min = 10, n_max = 30, a_n = 1, b_n = 1, #data
                 n_assignment_method = "ascending", 
                 rankPriority = "top", rankSteepness = .9, #rankWeights
-                parameter = NULL, loss = 2, f=identity, rankweights = "", #ranking settings
+                parameter = NULL, loss = l, f=identity, rankweights = "", #ranking settings
                 n_sim = 1,
                 fileRoot = "/Users/cora/git_repos/RankingMethods/results/",
                 metric = FALSE, metricFile = "/Users/cora/git_repos/RankingMethods/results/metricResults.csv")
+  }
 }
