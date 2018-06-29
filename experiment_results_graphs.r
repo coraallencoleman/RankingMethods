c3a62724fe84dfc3910a4770814a8bff06e4c746
Cora Allen-Coleman <coraallencoleman@gmail.com>
2018-06-29 12:33:31 -0500

    finished graphs and ran n = 10 and n=100 on nebula-8 and nebula-9 respectively

--- experiment_results_graphs.r
+++ experiment_results_graphs.r
@@ -4,37 +4,79 @@
 # could plot total loss or some other kind of metric
 
 require(ggplot2)
-
+setwd("/Users/cora/git_repos/RankingMethods/plots/")
 ## data cleaning ##
+#load("~/gangnon/results/ranking_experiment_results.RData")
 load("/Users/cora/git_repos/RankingMethods/results/ranking_experiment_results.RData") #called df
 results <- as.data.frame(df)
 results[, c(1:8, 11, 14)] <- sapply( results[,c(1:8, 11, 14)], as.character )
 results[, c(1:8, 11, 14)] <- sapply( results[,c(1:8, 11, 14)], as.double )
 
 ## TOTAL LOSS ##
-n_min_plot <- ggplot(results, aes(x = n_min, y = totalLoss, colour = factor(n_max))) + geom_point() + 
-  geom_line() + ggtitle("Avg Total Loss By Changes in Binomial n min, max") + 
+postscript("loss_nmin_nmax_plot.eps")
+n_min_plot <- ggplot(results, aes(x = n_min, y = totalLoss, colour = factor(n_max))) + geom_jitter(width = 20) + 
+  ggtitle("Avg Total Loss by Changes in Binomial n min, max") + 
   ylab("Total Loss"); n_min_plot
+dev.off()
 
-changes_in_N_plot <- ggplot(results, aes(x = n_min, y = totalLoss, colour = factor(N))) + geom_point() + 
-  ggtitle("Avg Total Loss By Changes in N") + 
+postscript("loss_nmin_n_plot.eps")
+changes_in_N_plot <- ggplot(results, aes(x = n_min, y = totalLoss, colour = factor(N))) + geom_jitter(width = 20) + 
+  ggtitle("Avg Total Loss by Changes in N") + 
   ylab("Total Loss"); changes_in_N_plot
+dev.off()
 
-losses_plot <- ggplot(results, aes(x = N, y = totalLoss, colour = factor(loss))) + geom_point() + 
-  ggtitle("Avg Total Loss By Changes in N") + 
-  ylab("Total Loss"); losses_plot
+# postscript("loss_n_loss_plot.eps")
+# losses_plot <- ggplot(results, aes(x = N, y = totalLoss, colour = factor(loss))) + 
+#   geom_jitter(width = 5) + 
+#   ggtitle("Avg Total Loss By Changes in N") + 
+#   ylab("Total Loss"); losses_plot
+# dev.off()
 
+postscript("loss_loss_rankPriority_plot.eps")
+rankPriority_plot <- ggplot(results, aes(x = factor(loss), y = totalLoss, colour = factor(rankPriority))) + 
+  geom_jitter(width = .15) + 
+  ggtitle("Avg Total Loss by Changes in Loss Type and Rank Priority Weighting") + 
+  ylab("Total Loss"); rankPriority_plot
+dev.off()
+
+postscript("loss_loss_rankPriority_plot.eps")
+rankPriority_plot <- ggplot(results, aes(x = rankSteepness, y = metric, colour = factor(rankPriority))) + 
+  geom_jitter(width = .005) +
+  ggtitle("Avg Total Loss by \nChanges in Loss and Rank Priority") +
+  ylab("Percent Correct") + xlab("Rank Steepness") + xlim(0, 1); rankPriority_plot
+dev.off()
 
 ## RANKING QUALITY METRIC ##
-n_min_plot <- ggplot(results, aes(x = n_min, y = metric, colour = factor(n_min))) + geom_point() + 
-  ggtitle("Percent Correct Ranking By \nChanges in Binomial n min, max") + 
-  ylab("Percent Correct"); n_min_plot
 
-changes_in_N_plot <- ggplot(results, aes(x = N, y = metric, colour = factor(n_min))) + geom_point() + 
-  ggtitle("Percent Correct Ranking By \nChanges in N") + 
-  ylab("Percent Correct"); changes_in_N_plot
+postscript("metric_nmin_nmax_plot.eps")
+n_min_plot <- ggplot(results, aes(x = n_min, y = metric, colour = factor(n_max))) + 
+  geom_jitter(width = 10) + 
+  ggtitle("Percent Correct Ranking by \nChanges in Binomial n min, max") + 
+  ylab("Percent Correct") + xlab("n min"); n_min_plot
+dev.off()
 
-losses_plot <- ggplot(results, aes(x = N, y = metric, colour = factor(loss))) + geom_point() + 
-  ggtitle("Percent Correct Ranking By Changes in N and loss") + 
-  ylab("Percent Correct"); losses_plot
+# postscript("metric_n_nmax_plot.eps")
+# changes_in_N_plot <- ggplot(results, aes(x = N, y = metric, colour = factor(n_min))) + 
+#   geom_jitter(width = 10) + 
+#   ggtitle("Percent Correct Ranking \nBy Changes in N") + 
+#   ylab("Percent Correct") + xlab("N"); changes_in_N_plot
+# dev.off()
 
+# losses_plot <- ggplot(results, aes(x = N, y = metric, colour = factor(loss))) + 
+#   geom_jitter(width = 5) + 
+#   ggtitle("Percent Correct Ranking \nBy Changes in N and loss") + 
+#   ylab("Percent Correct") + xlab("N"); losses_plot
+
+postscript("metric_loss_rankPriority_plot.eps")
+rankPriority_plot <- ggplot(results, aes(x = factor(loss), y = metric, colour = factor(rankPriority))) + 
+  geom_jitter(width = .15) +
+  ggtitle("Percent Correct Ranking by \nChanges in Loss and Rank Priority") +
+  ylab("Percent Correct") + xlab("Loss"); rankPriority_plot
+dev.off()
+
+postscript("metric_loss_rankPriority_plot.eps")
+rankPriority_plot <- ggplot(results, aes(x = rankSteepness, y = metric, colour = factor(rankPriority))) + 
+  geom_jitter(width = .005) +
+  ggtitle("Percent Correct Ranking by \nChanges in Loss and Rank Priority") +
+  ylab("Percent Correct") + xlab("Rank Steepness") + xlim(0, 1); rankPriority_plot
+dev.off()
--- ranking_experiment_run_server_n100.r
+++ ranking_experiment_run_server_n100.r
@@ -4,10 +4,10 @@
 # run in gangnon/ranking dir 
 
 # move script to server:
-#scp /Users/cora/git_repos/RankingMethods/*.r allencoleman@adhara.biostat.wisc.edu:/ua/allencoleman/gangnon/ranking/
+#scp /Users/cora/git_repos/RankingMethods/ranking_experiment_run_server_n100.r allencoleman@adhara.biostat.wisc.edu:/ua/allencoleman/gangnon/ranking/
 
 # run:
-#nohup /s/pkg/linux64/R/3.4.1/bin/Rscript ranking_experiment_run_server.r > nsim10ScreenLog.txt &
+#nohup /s/pkg/linux64/R/3.4.1/bin/Rscript ranking_experiment_run_server_n100.r > nsim100ScreenLog.txt &
 
 # move results back to home computer 
 #scp allencoleman@adhara.biostat.wisc.edu:/ua/allencoleman/gangnon/ranking/results/* /Users/cora/git_repos/RankingMethods/results/ 
@@ -42,7 +42,7 @@ for (n in c(100)){ #numItems
                 rankPriority = rankPriority, rankSteepness = rankSteepness, #rankWeights
                 parameter = NULL, loss = l, 
                 f=identity,  #ranking settings
-                n_sim = 10, #100 or 1000 depending on time
+                n_sim = 100, #100 or 1000 depending on time
                 #fileRoot = "/Users/cora/git_repos/RankingMethods/results/",
                 fileRoot = "/ua/allencoleman/gangnon/ranking/results/",
                 metric = TRUE))
@@ -57,37 +57,6 @@ for (n in c(100)){ #numItems
 #CAREFUL! THIS OVERWRITES
 df <- apply(results,2,as.character)
 #save(df, file = "/Users/cora/git_repos/RankingMethods/results/ranking_experiment_results.RData") #saves as an R object
-save(df, file = "/ua/allencoleman/gangnon/ranking/results/ranking_experiment_results_n10.RData")
-
-#data frames of lists
-# have an element of df be a list or matrix. We want rankings to be a matrix within a list, one for each simulation
-#for every simulation, we need a matrix of ranks
-# save as an R object, not csv.
-#look for this discussion in tidyverse data science R book
-
-# load("/Users/cora/git_repos/RankingMethods/results/ranking_experiment_results.RData") 
-# head(df)
-
-##Main Questions for this Experiment:
-##increase gaps in parameters until they're trivial. decrease until broken/impossible. 
-#think of this as an experiment. what experimental conditions do we need to run to get a sense of behavior?
-
-
-### Simulations TODO
-# variation in N
-# different losses
-# parameter scale probability, rank, logit scale
-# weights vs no weights (part of ranking)
-
-#DATA
-# even gaps
-# uneven gaps #qbeta(1:N/(N+1), 1, 1). Vary the last two parameters for variable gap size
-#p c()# look at beta distributions for alpha and beta. could crowd them up at one edge. will be symmetric
-#n
-#for below, think about how big average n should be to get p in CI
-#n min: c(50) explore this to see . what differences in p1 and p2 coudl you find with this n? do a power calculation basically
-#n max: se = sqrt(p*q/n). think about how small this would have to be to think about how big n needs to be
-#se = sqrt((.1*.9)/20) #graph this out to see. If the ranking is always right, then n is too big. If ranks are uniform/terrible, then n too small.
-# assignment method
+save(df, file = "/ua/allencoleman/gangnon/ranking/results/ranking_experiment_results_n100.RData")
 
 #system(mutt -s "Master, this is an email from HAL 9000, your commands have been completed" allencoleman@wisc.edu < ~/R/output.txt)
