a83d4aea8bd3fab707e8f443c8275fd06f2cc8bd
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2018-10-01 10:21:45 -0500

    added Ron's code and csv for ranking real WI LBW data

---
+++ wi_ranking_Ron_092418.R
@@ -0,0 +1,93 @@
+library(tidyverse)
+library(coda)
+
+setwd("/Users/cora/git_repos/RankingMethods/")
+
+lbw_wi <- read_csv("lbw_wi.csv")
+#remove missing, then create N normal birth weight, % LBW columns
+lbw_wi <- lbw_wi %>% mutate(nbw=births-lbw,perc_lbw=lbw/births*100) %>% filter(!is.na(lbw))
+
+#sample from LBW beta distribution using lbw as shape1, nbw as shape2 Q
+lbw_samples <- replicate(10000,
+                         rbeta(n = 71,shape1=lbw_wi$lbw,shape2=lbw_wi$nbw))
+lbw_ranks <- apply(lbw_samples,2,rank) #Q doesnt this flip? I use 1 here in WeightedLossRanking function
+lbw_order <- apply(lbw_samples,2,sort)
+
+#creates vectors of HPD intervals (length = 2*71)
+lbw_HPD <- t(apply(lbw_samples,1,function(x) HPDinterval(mcmc(x)))) 
+lbw_rank_HPD <- t(apply(lbw_ranks,1,function(x) HPDinterval(mcmc(x))))
+lbw_order_HPD <- t(apply(lbw_order,1,function(x) HPDinterval(mcmc(x))))
+
+SEL_rank <- matrix(NA,71,71) #what does SEL stand for? 
+for (i in 1:71) {
+  for (j in 1:71) {
+    SEL_rank[i,j] <- mean((lbw_ranks[i,]-j)^2)
+  }
+}
+
+SEL_prob <- matrix(NA,71,71)
+for (i in 1:71) {
+  for (j in 1:71) {
+    SEL_prob[i,j] <- mean((lbw_samples[i,]-lbw_order[j,])^2)
+  }
+}
+
+lbw_rank_pm <- apply(lbw_ranks,1,mean)
+lbw_rank_SEL_rank_ind_opt <- apply(SEL_rank,1,which.min)
+lbw_rank_SEL_rank_joint_opt <- as.numeric(solve_LSAP(SEL_rank))
+
+lbw_rank_SEL_prob_ind_opt <- apply(SEL_prob,1,which.min)
+lbw_rank_SEL_prob_joint_opt <- as.numeric(solve_LSAP(SEL_prob))
+
+
+
+
+prob1 <- matrix(NA,71,71)
+for (i in 1:71) {
+  for (j in 1:71) {
+    prob1[i,j] <- mean(lbw_samples[i,]<lbw_samples[j,])
+  }
+}
+#prob1a <- prob1[order(rank2a),order(rank2a)]
+
+
+
+lbw_results_selr <- data_frame(county=lbw_wi$county,
+                          LBW_pm=apply(lbw_samples,1,mean),
+                          LBW_LCL=lbw_HPD[,1],
+                          LBW_UCL=lbw_HPD[,2],
+                          LBW_rank_pm=lbw_rank_pm,
+                          LBW_rank_io=lbw_rank_SEL_rank_ind_opt,
+                          LBW_rank_jo=lbw_rank_SEL_rank_joint_opt,
+                          LBW_rank_LCL=lbw_rank_HPD[,1],
+                          LBW_rank_UCL=lbw_rank_HPD[,2]) %>% 
+  arrange(LBW_rank_jo)
+
+lbw_results_selp <- data_frame(county=lbw_wi$county,
+                               LBW_pm=apply(lbw_samples,1,mean),
+                               LBW_LCL=lbw_HPD[,1],
+                               LBW_UCL=lbw_HPD[,2],
+                               LBW_rank_io=lbw_rank_SEL_prob_ind_opt,
+                               LBW_rank_jo=lbw_rank_SEL_prob_joint_opt,
+                               LBW_rank_LCL=lbw_rank_HPD[,1],
+                               LBW_rank_UCL=lbw_rank_HPD[,2]) %>% 
+  arrange(LBW_rank_jo)
+
+post_ranks <- t(apply(lbw_ranks,1,function(x) table(factor(x,levels=1:71))/max(table(factor(x,levels=1:71)))))
+#post_ranks <- t(apply(lbw_ranks,1,function(x) table(factor(x,levels=1:71))/10000))
+#post_ranks <- post_ranks/max(post_ranks)
+post_df <- melt(post_ranks)
+post_df$county <- lbw_wi$county[post_df$Var1]
+post_df$rank <- post_df$Var2
+post_df$pos <- lbw_order_stats[post_df$Var2]
+post_df$county <- factor(post_df$county,levels=rev(lbw_wi$county[order(lbw_rank_SEL_prob_joint_opt)]),ordered=TRUE)
+
+ggplot(post_df,aes(x=rank,y=county,color=value))+
+  geom_point(pch=15,cex=2)+
+  scale_y_discrete("") +
+  scale_x_continuous("",breaks=seq(1,71,by=5))+
+#  scale_x_continuous(breaks=lbw_order_stats[c(1:7,seq(10,60,by=10),67:71)],minor_breaks=lbw_order_stats,labels=c(1:7,seq(10,60,by=10),67:71))+
+  scale_color_gradient(low="white",high="black",limits=c(0,1),guide=FALSE)+
+  theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+
+
